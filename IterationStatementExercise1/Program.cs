0359f64c7a0a2ba2157738cb714cd40d9d53388d
William Thompson <wmthmps1@gmail.com>
2023-12-11 21:36:15 -0600

    Initial Commit

---
+++ IterationStatementExercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
